89de6f78f991a157c79097555e6dd08e4777471b
Tom <tomjuggler@gmail.com>
2021-03-11 22:20:05 +0200

    using state to add up beam and PIR events
    Signed-off-by: Tom <tomjuggler@gmail.com>

--- ProBeams.ino
+++ ProBeams.ino
@@ -12,43 +12,67 @@ long previousMillis = 0;
 int val = 0;  // variable to store the sensor status (value)
 int reading = 0; //beamPin reading
 
+//save events counter:
+int PIREvents = 0;
+int beamEvents = 0;
+int state = LOW;
+
 void setup() {
-  Serial.begin(9600);  
+  Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
   pinMode(beamPin, INPUT);
   pinMode(PIRPin, INPUT);
-  //test LED:  
-    for (int i = 0; i < 3; i++) {
-      digitalWrite(ledPin, HIGH);
-      delay(200);
-      digitalWrite(ledPin, LOW);
-    }  
+  //test LED:
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(ledPin, HIGH);
+    delay(200);
+    digitalWrite(ledPin, LOW);
+  }
 }
 
 void loop() {
-reading = digitalRead(beamPin);
-val = digitalRead(PIRPin);
+  reading = digitalRead(beamPin);
+  val = digitalRead(PIRPin);
   if (millis() - previousMillis > 50) { //only check every 50ms? seems to work great!
-    if (reading==HIGH || val==HIGH) {
-      Serial.println("high");
+    if (reading == HIGH || val == HIGH) {
+      if (state == LOW) {
+        if (reading == HIGH) {
+          beamEvents++;
+        }
+        if (val == HIGH) {
+          PIREvents++;
+        }
+        Serial.print("Motion detected! PIR: " );
+        Serial.print(PIREvents);
+        Serial.print(", beamEvents: ");
+        Serial.println(beamEvents);
+
+        state = HIGH;       // update variable state to HIGH
+      }
+
+      //      Serial.println("high");
       digitalWrite(ledPin, HIGH); //switch on LED if triggered
       /*
-      for(int i = 0; i < 5000; i++){
-      tone(piezoPin, 1000*i, 5000);
-      tone(piezoPin, 200, 5000);
-      tone(piezoPin, 300000, 5000);
-      }
-      //crazy long alarm beeps:
-     for(int i = 0; i < 20; i++){
-      tone(piezoPin, 1000*i, 5000);
-      delay(5000);
-      tone(piezoPin, 200, 50000);
-     tone(piezoPin, 300000, 500);
-      }
+        for(int i = 0; i < 5000; i++){
+        tone(piezoPin, 1000*i, 5000);
+        tone(piezoPin, 200, 5000);
+        tone(piezoPin, 300000, 5000);
+        }
+        //crazy long alarm beeps:
+        for(int i = 0; i < 20; i++){
+        tone(piezoPin, 1000*i, 5000);
+        delay(5000);
+        tone(piezoPin, 200, 50000);
+        tone(piezoPin, 300000, 500);
+        }
       */
-       } else {
-        Serial.println("low");
-      digitalWrite(ledPin, LOW); //if you want the LED to switch off after it is triggered. Otherwise the LED stays on. 
+    } else {
+      if (state == HIGH) {
+        Serial.println("Motion stopped!");
+        state = LOW;       // update variable state to LOW
+      }
+      //        Serial.println("low");
+      digitalWrite(ledPin, LOW); //if you want the LED to switch off after it is triggered. Otherwise the LED stays on.
     }
     previousMillis = millis();
   }
